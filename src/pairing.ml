879f514c45e3e6e442b07ed95d319a4b89beec67
Danny Willems <be.danny.willems@gmail.com>
2020-08-18 16:35:09 +0200

    Add doc and final_exponentiation_exn raises an exception instead of returning an option

--- src/pairing.ml
+++ src/pairing.ml
@@ -5,13 +5,21 @@ module type T = sig
 
   module GT : Ff.T
 
+  exception FailToComputeFinalExponentiation of GT.t
+
   val miller_loop : (G1.t * G2.t) list -> GT.t
 
+  (** Compute the miller loop on a single tuple of point *)
   val miller_loop_simple : G1.t * G2.t -> GT.t
 
+  (** Compute a pairing result of a list of points *)
   val pairing : (G1.t * G2.t) list -> GT.t
 
+  (** Compute the final exponentiation of the given point. Returns a [None] if
+      the point is null *)
   val final_exponentiation_opt : GT.t -> GT.t option
 
-  val final_exponentiation_exn : GT.t -> GT.t option
+  (** Compute the final exponentiation of the given point. Raise
+      [FailToComputeFinalExponentiation] if the point is null *)
+  val final_exponentiation_exn : GT.t -> GT.t
 end

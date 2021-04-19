59ace338a907d2216217daa42054015f6fb66f55
Danny Willems <be.danny.willems@gmail.com>
2021-04-19 12:16:36 +0200

    Test random points are not on the curve

--- test/test_babyjubjub_reduced.ml
+++ test/test_babyjubjub_reduced.ml
@@ -6,8 +6,39 @@ module EdwardsCurveProperties =
   Ec_pbt.MakeEdwardsCurveProperties (Ec_babyjubjub_reduced.Affine)
 
 let test_random_is_not_small_order () =
-  Ec_pbt.repeat 1000 (fun () ->
-      assert (not Ec_babyjubjub_reduced.Affine.(is_small_order (random ()))))
+  assert (not Ec_babyjubjub_reduced.Affine.(is_small_order (random ())))
+
+let test_random_points_not_on_curve () =
+  (* pick random values u and v and test constructors fail *)
+  let u = Ec_babyjubjub_reduced.Affine.BaseField.random () in
+  let v = Ec_babyjubjub_reduced.Affine.BaseField.random () in
+  let bytes =
+    Bytes.concat
+      Bytes.empty
+      [ Ec_babyjubjub_reduced.Affine.BaseField.to_bytes u;
+        Ec_babyjubjub_reduced.Affine.BaseField.to_bytes v ]
+  in
+  (* check_bytes *)
+  assert (not (Ec_babyjubjub_reduced.Affine.check_bytes bytes)) ;
+  (* of_bytes_opt *)
+  assert (Option.is_none (Ec_babyjubjub_reduced.Affine.of_bytes_opt bytes)) ;
+  (* of_bytes_exn *)
+  ( try
+      ignore (Ec_babyjubjub_reduced.Affine.of_bytes_exn bytes) ;
+      assert false
+    with
+  | Ec_babyjubjub_reduced.Affine.Not_on_curve _ -> ()
+  | _ -> assert false ) ;
+  (* from_coordinates_opt *)
+  assert (
+    Option.is_none (Ec_babyjubjub_reduced.Affine.from_coordinates_opt ~u ~v) ) ;
+  (* from_coordinates_exn *)
+  try
+    ignore (Ec_babyjubjub_reduced.Affine.from_coordinates_exn ~u ~v) ;
+    assert false
+  with
+  | Ec_babyjubjub_reduced.Affine.Not_on_curve _ -> ()
+  | _ -> assert false
 
 let () =
   let open Alcotest in
@@ -18,7 +49,11 @@ let () =
         [ Alcotest.test_case
             "test random elements are in the prime subgroup"
             `Quick
-            (test_random_is_not_small_order ()) ] );
+            (Ec_pbt.repeat 100 test_random_is_not_small_order);
+          Alcotest.test_case
+            "test random coordinates u, v do not give a point on the curve"
+            `Quick
+            (Ec_pbt.repeat 100 test_random_points_not_on_curve) ] );
       ValueGeneration.get_tests ();
       EdwardsCurveProperties.get_tests ();
       Properties.get_tests ();
--- test/test_jubjub.ml
+++ test/test_jubjub.ml
@@ -5,8 +5,38 @@ module EdwardsCurveProperties =
   Ec_pbt.MakeEdwardsCurveProperties (Ec_jubjub.Affine)
 
 let test_random_is_not_small_order () =
-  Ec_pbt.repeat 1000 (fun () ->
-      assert (not Ec_jubjub.Affine.(is_small_order (random ()))))
+  assert (not Ec_jubjub.Affine.(is_small_order (random ())))
+
+let test_random_points_not_on_curve () =
+  (* pick random values u and v and test constructors fail *)
+  let u = Ec_jubjub.Affine.BaseField.random () in
+  let v = Ec_jubjub.Affine.BaseField.random () in
+  let bytes =
+    Bytes.concat
+      Bytes.empty
+      [ Ec_jubjub.Affine.BaseField.to_bytes u;
+        Ec_jubjub.Affine.BaseField.to_bytes v ]
+  in
+  (* check_bytes *)
+  assert (not (Ec_jubjub.Affine.check_bytes bytes)) ;
+  (* of_bytes_opt *)
+  assert (Option.is_none (Ec_jubjub.Affine.of_bytes_opt bytes)) ;
+  (* of_bytes_exn *)
+  ( try
+      ignore (Ec_jubjub.Affine.of_bytes_exn bytes) ;
+      assert false
+    with
+  | Ec_jubjub.Affine.Not_on_curve _ -> ()
+  | _ -> assert false ) ;
+  (* from_coordinates_opt *)
+  assert (Option.is_none (Ec_jubjub.Affine.from_coordinates_opt ~u ~v)) ;
+  (* from_coordinates_exn *)
+  try
+    ignore (Ec_jubjub.Affine.from_coordinates_exn ~u ~v) ;
+    assert false
+  with
+  | Ec_jubjub.Affine.Not_on_curve _ -> ()
+  | _ -> assert false
 
 let () =
   let open Alcotest in
@@ -17,7 +47,12 @@ let () =
         [ Alcotest.test_case
             "test random elements are in the prime subgroup"
             `Quick
-            (test_random_is_not_small_order ()) ] );
+            (Ec_pbt.repeat 1000 test_random_is_not_small_order) ] );
+      ( "Tests random",
+        [ Alcotest.test_case
+            "test random coordinates do not give a point on the curve"
+            `Quick
+            (Ec_pbt.repeat 100 test_random_points_not_on_curve) ] );
       ValueGeneration.get_tests ();
       EdwardsCurveProperties.get_tests ();
       Properties.get_tests ();

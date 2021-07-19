fc39236c1017973d5f8304aa9559ba230af426b2
Danny Willems <be.danny.willems@gmail.com>
2021-07-19 11:44:32 +0200

    Add documentation in Poseidon.Core

--- src/poseidon/core.mli
+++ src/poseidon/core.mli
@@ -1,26 +1,37 @@
 module type PARAMETERS = sig
+  (** The state size *)
   val width : int
 
+  (** The total number of full rounds *)
   val full_rounds : int
 
+  (** The number of partial rounds *)
   val partial_rounds : int
 
+  (** The round constants, given in decimal representation *)
   val round_constants : string array
 
+  (** The MDS matrix, given in decimal representation *)
   val mds_matrix : string array array
 
+  (** The index of the element of the state to permute during the partial rounds *)
   val partial_round_idx_to_permute : int
 end
 
+(** A HADES strategy, for a constant length construction *)
 module type STRATEGY = sig
   type scalar
 
+  (** The state of the strategy *)
   type state
 
+  (** Initialize the state with the given input. The input must be the same length than the width *)
   val init : scalar array -> state
 
+  (** Apply a permutation round *)
   val apply_perm : state -> unit
 
+  (** Return the current scalar elements in the state *)
   val get : state -> scalar array
 end
 
@@ -29,10 +40,15 @@ module type HASH = sig
 
   type ctxt
 
+  (** Initialize a raw hash context *)
   val init : unit -> ctxt
 
+  (** [hash ctxt input] computes the hash of the given input. The input must be
+      of length [width - 1]
+  *)
   val hash : ctxt -> scalar array -> ctxt
 
+  (** [get ctxt] returns the resulting point after [hash] has been called *)
   val get : ctxt -> scalar
 end
 

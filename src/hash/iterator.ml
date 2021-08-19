a09e295bf8f897c7d38115f23cf2215660c6efc1
Danny Willems <be.danny.willems@gmail.com>
2021-08-19 17:50:17 +0200

    Iterator: add internal state and get_chunk

--- src/hash/iterator.ml
+++ src/hash/iterator.ml
@@ -1,11 +1,13 @@
 type bit = int
 
 module Bit = struct
-  type t = unit -> bit option
+  type state = Pending | Processed
+
+  type t = { f : unit -> bit option; mutable state : state }
 
   let of_bytes_le b =
     let length = Bytes.length b in
-    if length = 0 then fun () -> None
+    if length = 0 then { f = (fun () -> None); state = Processed }
     else
       let current_byte_int = ref (Bytes.get_uint8 b 0) in
       let i = ref 0 in
@@ -23,18 +25,33 @@ module Bit = struct
           current_byte_int := !current_byte_int lsr 1 ;
           Some b
       in
-      f
+      { f; state = Pending }
+
+  let is_processed { f = _; state } =
+    match state with Processed -> true | _ -> false
 
   let create_from_bool_list bs =
     let bs_ref = ref bs in
-    let f () =
-      match !bs_ref with
-      | [] -> None
-      | b :: bs ->
-          bs_ref := bs ;
-          Some (if b then 1 else 0)
-    in
-    f
+    let length = List.length bs in
+    if length = 0 then { f = (fun () -> None); state = Processed }
+    else
+      let f () =
+        match !bs_ref with
+        | [] -> None
+        | b :: bs ->
+            bs_ref := bs ;
+            Some (if b then 1 else 0)
+      in
+      { f; state = Pending }
 
-  let next i = i ()
+  let next iterator =
+    if is_processed iterator then None
+    else
+      let res = iterator.f () in
+      (match res with None -> iterator.state <- Processed | _ -> ()) ;
+      res
+
+  let get_chunk iterator ?(default = 0) n =
+    if is_processed iterator then List.init n (fun _ -> default)
+    else List.init n (fun _ -> Option.value (next iterator) ~default)
 end
--- test/test_iterator.ml
+++ test/test_iterator.ml
@@ -91,6 +91,48 @@ let test_multiple_bytes () =
       assert (Iterator.Bit.next iterator = None))
     test_vectors
 
+let test_is_processed () =
+  let test_vectors =
+    [ ([], [(0, true); (1, true); (Random.int 1000, true)]);
+      ([true], [(0, false); (1, false); (2, true); (2 + Random.int 1000, true)]);
+      ([true; false; true], [(Random.int 3, false); (3 + Random.int 1000, true)]);
+      (let n = Random.int 1000 in
+       ( List.init n (fun _ -> Random.bool ()),
+         [(Random.int (n + 1), false); (n + 1 + Random.int 1000, true)] ));
+      ([true; false; true], [(Random.int 3, false); (3 + Random.int 1000, true)])
+    ]
+  in
+  List.iter
+    (fun (v, iterations) ->
+      List.iter
+        (fun (n, expected_value) ->
+          let iterator = Iterator.Bit.create_from_bool_list v in
+          for _ = 0 to n - 1 do
+            ignore @@ Iterator.Bit.next iterator
+          done ;
+          assert (Iterator.Bit.is_processed iterator = expected_value))
+        iterations)
+    test_vectors
+
+let test_get_chunk () =
+  let test_vectors =
+    [ ([], 5, [List.init 5 (fun _ -> 0)]);
+      ([true; false], 1, [[1]; [0]]);
+      ([true; false], 2, [[1; 0]; [0; 0]]);
+      ([true; false; true], 2, [[1; 0]; [1; 0]]);
+      ([true; false; true; true], 2, [[1; 0]; [1; 1]]);
+      ([true; false; true; true], 3, [[1; 0; 1]; [1; 0; 0]]) ]
+  in
+  List.iter
+    (fun (l, chunk_size, exp_values) ->
+      let iterator = Iterator.Bit.create_from_bool_list l in
+      List.iter
+        (fun exp_value ->
+          let chunk = Iterator.Bit.get_chunk iterator chunk_size in
+          assert (chunk = exp_value))
+        exp_values)
+    test_vectors
+
 let () =
   Alcotest.run
     ~verbose:true
@@ -99,4 +141,7 @@ let () =
         [ Alcotest.test_case "One byte" `Quick test_one_byte;
           Alcotest.test_case "Multiple bytes" `Quick test_multiple_bytes ] );
       ( "Bool list",
-        [Alcotest.test_case "Test vectors" `Quick test_from_bool_list] ) ]
+        [Alcotest.test_case "Test vectors" `Quick test_from_bool_list] );
+      ( "Utils functions",
+        [Alcotest.test_case "is processed" `Quick test_is_processed] );
+      ("get chunk", [Alcotest.test_case "get chnk" `Quick test_get_chunk]) ]

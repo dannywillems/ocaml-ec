061a48b25ff53d9945ed184b38e2f779a070b85f
Danny Willems <be.danny.willems@gmail.com>
2021-10-08 11:10:42 +0200

    Benchmark Poseidon instances: bench one permutation only

--- benches/bench_poseidon252.ml
+++ benches/bench_poseidon252.ml
@@ -2,22 +2,27 @@ open Core
 open Core_bench
 open Mec.Hash
 
-(* module Scalar = Ff.MakeFp (struct
- *   let prime_order =
- *     Z.of_string
- *       "52435875175126190479447740508185965837690552500527637822603658699938581184513"
- * end) *)
+module Scalar = Ff.MakeFp (struct
+  let prime_order =
+    Z.of_string
+      "52435875175126190479447740508185965837690552500527637822603658699938581184513"
+end)
 
-module Scalar = Bls12_381.Fr
 module Poseidon = Poseidon252.Make (Scalar)
 
 let t1 =
   let n = 5 in
   let inputs = Array.init n ~f:(fun _i -> Scalar.random ()) in
-  Bench.Test.create ~name:"Benchmark Poseidon with blst backend" (fun () ->
-      let ctxt = Poseidon.Hash.init () in
-      let ctxt = Poseidon.Hash.digest ctxt inputs in
-      let _v = Poseidon.Hash.get ctxt in
+  let name =
+    Printf.sprintf
+      "Benchmark one permutation of Poseidon252 with ocaml-ff backend on an \
+       input of %d elements"
+      n
+  in
+  Bench.Test.create ~name (fun () ->
+      let ctxt = Poseidon.Strategy.init inputs in
+      let () = Poseidon.Strategy.apply_perm ctxt in
+      let _v = Poseidon.Strategy.get ctxt in
       ())
 
 let command = Bench.make_command [t1]

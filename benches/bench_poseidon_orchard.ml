d078d9bf84e83a1d77b4e4d798b38398f37b175e
Danny Willems <be.danny.willems@gmail.com>
2021-10-08 16:32:20 +0200

    Benchmark Poseidon Orchard (Poseidon128)

---
+++ benches/bench_poseidon_orchard.ml
@@ -0,0 +1,30 @@
+open Core
+open Core_bench
+open Mec.Hash
+
+module Scalar = Ff.MakeFp (struct
+  let prime_order =
+    Z.of_string
+      "52435875175126190479447740508185965837690552500527637822603658699938581184513"
+end)
+
+module Poseidon = Poseidon128.Make (Scalar)
+
+let t1 =
+  let n = 3 in
+  let inputs = Array.init n ~f:(fun _i -> Scalar.random ()) in
+  let name =
+    Printf.sprintf
+      "Benchmark one permutation of Poseidon128 (Orchard parameters) with \
+       ocaml-ff backend on an input of %d elements"
+      n
+  in
+  Bench.Test.create ~name (fun () ->
+      let ctxt = Poseidon.Strategy.init inputs in
+      let () = Poseidon.Strategy.apply_perm ctxt in
+      let _v = Poseidon.Strategy.get ctxt in
+      ())
+
+let command = Bench.make_command [t1]
+
+let () = Core.Command.run command

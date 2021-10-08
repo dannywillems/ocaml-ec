8c6be1b63cca95ae190ccc8a21705c62e4e9abc9
Danny Willems <be.danny.willems@gmail.com>
2021-10-08 16:57:29 +0200

    Poseidon inplace: add benches

--- benches/bench_poseidon252.ml
+++ benches/bench_poseidon252.ml
@@ -2,16 +2,14 @@ open Core
 open Core_bench
 open Mec.Hash
 
-module Scalar = Ff.MakeFp (struct
-  let prime_order =
-    Z.of_string
-      "52435875175126190479447740508185965837690552500527637822603658699938581184513"
-end)
-
-module Poseidon = Poseidon252.Make (Scalar)
-
 let t1 =
-  let n = 3 in
+  let module Scalar = Ff.MakeFp (struct
+    let prime_order =
+      Z.of_string
+        "52435875175126190479447740508185965837690552500527637822603658699938581184513"
+  end) in
+  let module Poseidon = Poseidon252.Make (Scalar) in
+  let n = 5 in
   let inputs = Array.init n ~f:(fun _i -> Scalar.random ()) in
   let name =
     Printf.sprintf
@@ -25,6 +23,39 @@ let t1 =
       let _v = Poseidon.Strategy.get ctxt in
       ())
 
-let command = Bench.make_command [t1]
+let t2 =
+  let module Scalar = Bls12_381.Fr in
+  let module Poseidon = Poseidon252.Make (Scalar) in
+  let n = 5 in
+  let inputs = Array.init n ~f:(fun _i -> Scalar.random ()) in
+  let name =
+    Printf.sprintf
+      "Benchmark one permutation of Poseidon252 with blst backend on an input \
+       of %d elements"
+      n
+  in
+  Bench.Test.create ~name (fun () ->
+      let ctxt = Poseidon.Strategy.init inputs in
+      let () = Poseidon.Strategy.apply_perm ctxt in
+      let _v = Poseidon.Strategy.get ctxt in
+      ())
+
+let t3 =
+  let module Poseidon = Poseidon252.MakeInplace (Bls12_381.Fr) in
+  let n = 5 in
+  let inputs = Array.init n ~f:(fun _i -> Bls12_381.Fr.random ()) in
+  let name =
+    Printf.sprintf
+      "Benchmark one permutation of Poseidon252 with blst backend and using \
+       inplace operations on an input of %d elements"
+      n
+  in
+  Bench.Test.create ~name (fun () ->
+      let ctxt = Poseidon.Strategy.init inputs in
+      let () = Poseidon.Strategy.apply_perm ctxt in
+      let _v = Poseidon.Strategy.get ctxt in
+      ())
+
+let command = Bench.make_command [t1; t2; t3]
 
 let () = Core.Command.run command
--- benches/bench_poseidon_orchard.ml
+++ benches/bench_poseidon_orchard.ml
@@ -2,15 +2,13 @@ open Core
 open Core_bench
 open Mec.Hash
 
-module Scalar = Ff.MakeFp (struct
-  let prime_order =
-    Z.of_string
-      "52435875175126190479447740508185965837690552500527637822603658699938581184513"
-end)
-
-module Poseidon = Poseidon128.Make (Scalar)
-
 let t1 =
+  let module Scalar = Ff.MakeFp (struct
+    let prime_order =
+      Z.of_string
+        "52435875175126190479447740508185965837690552500527637822603658699938581184513"
+  end) in
+  let module Poseidon = Poseidon128.Make (Scalar) in
   let n = 3 in
   let inputs = Array.init n ~f:(fun _i -> Scalar.random ()) in
   let name =
@@ -25,6 +23,39 @@ let t1 =
       let _v = Poseidon.Strategy.get ctxt in
       ())
 
-let command = Bench.make_command [t1]
+let t2 =
+  let module Scalar = Bls12_381.Fr in
+  let module Poseidon = Poseidon128.Make (Scalar) in
+  let n = 3 in
+  let inputs = Array.init n ~f:(fun _i -> Scalar.random ()) in
+  let name =
+    Printf.sprintf
+      "Benchmark one permutation of Poseidon128 (Orchard parameters) with blst \
+       backend on an input of %d elements"
+      n
+  in
+  Bench.Test.create ~name (fun () ->
+      let ctxt = Poseidon.Strategy.init inputs in
+      let () = Poseidon.Strategy.apply_perm ctxt in
+      let _v = Poseidon.Strategy.get ctxt in
+      ())
+
+let t3 =
+  let module Poseidon = Poseidon128.MakeInplace (Bls12_381.Fr) in
+  let n = 3 in
+  let inputs = Array.init n ~f:(fun _i -> Bls12_381.Fr.random ()) in
+  let name =
+    Printf.sprintf
+      "Benchmark one permutation of Poseidon128 (Orchard parameters) with \
+       inplace operations and blst backend on an input of %d elements"
+      n
+  in
+  Bench.Test.create ~name (fun () ->
+      let ctxt = Poseidon.Strategy.init inputs in
+      let () = Poseidon.Strategy.apply_perm ctxt in
+      let _v = Poseidon.Strategy.get ctxt in
+      ())
+
+let command = Bench.make_command [t1; t2; t3]
 
 let () = Core.Command.run command
